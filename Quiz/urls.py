46388e96a2ff5c11a13d09bee2c9062bc4a17700
sivasibu <sivas.margy@gmail.com>
2022-12-27 12:44:39 +0530

    initial commit

---
+++ Quiz/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",views.log,name="Home"),
+    path("register/",views.reg,name="register"),
+    path("test/",views.test,name="Test")
+]
